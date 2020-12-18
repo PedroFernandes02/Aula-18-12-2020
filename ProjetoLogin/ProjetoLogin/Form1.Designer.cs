9c30eaa7810fed8c86f44fd1df9cfdd08a3e5daf
PedroFernandes02 <75850937+PedroFernandes02@users.noreply.github.com>
2020-12-18 15:53:54 +0000

    Add files via upload

---
+++ ProjetoLogin/ProjetoLogin/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace ProjetoLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdEntrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textUtilizador = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmdEntrar
+            // 
+            this.cmdEntrar.Location = new System.Drawing.Point(70, 187);
+            this.cmdEntrar.Name = "cmdEntrar";
+            this.cmdEntrar.Size = new System.Drawing.Size(75, 23);
+            this.cmdEntrar.TabIndex = 2;
+            this.cmdEntrar.Text = "Entrar";
+            this.cmdEntrar.UseVisualStyleBackColor = true;
+            this.cmdEntrar.Click += new System.EventHandler(this.cmdEntrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Utilizador:";
+            this.label1.TextChanged += new System.EventHandler(this.textTextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            this.label2.TextChanged += new System.EventHandler(this.textTextChanged);
+            // 
+            // textUtilizador
+            // 
+            this.textUtilizador.Location = new System.Drawing.Point(67, 81);
+            this.textUtilizador.Name = "textUtilizador";
+            this.textUtilizador.Size = new System.Drawing.Size(100, 20);
+            this.textUtilizador.TabIndex = 0;
+            this.textUtilizador.TextChanged += new System.EventHandler(this.textTextChanged);
+            this.textUtilizador.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textUtilizador_KeyDown);
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(67, 136);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(100, 20);
+            this.textPassword.TabIndex = 1;
+            this.textPassword.TextChanged += new System.EventHandler(this.textTextChanged);
+            this.textPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textPassword_KeyDow);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjetoLogin.Properties.Resources.user;
+            this.pictureBox1.Location = new System.Drawing.Point(222, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 159);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textUtilizador);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdEntrar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdEntrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textUtilizador;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ProjetoLogin/ProjetoLogin/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoLogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmdEntrar.Enabled = false;
+        }
+
+        private void textTextChanged(object sender, EventArgs e)
+        {
+            cmdEntrar.Enabled = textUtilizador.Text != "" && textPassword.Text != "" ? true : false;
+        }
+
+        private void textUtilizador_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                textPassword.Focus();
+            }
+        }
+
+        private void textPassword_KeyDow(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                cmdEntrar_Click(this, EventArgs.Empty);
+            }
+
+        }
+
+        private void cmdEntrar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Entrou");
+        }
+    }
+}
